d0a56a0df6bd1657d39d2ff6bae6719d10b72e08
Gopa71 <143803192+Gopa71@users.noreply.github.com>
2023-09-13 12:16:52 +0530

    Add files via upload

---
+++ Buffer_controller.js
@@ -0,0 +1,6 @@
+
+singleButton({name:"Led On", action:"analogWrite", 
+              pin:"0", value:"255"})
+
+singleButton({name:"Led Off", action:"analogWrite", 
+              pin:"0", value:"0"})
